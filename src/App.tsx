56419b681ba51762795ed2f0efac58b997f48255
linda <linnppa@gmail.com>
2024-01-25 02:22:52 -0500

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Linda Quach CS5610
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
