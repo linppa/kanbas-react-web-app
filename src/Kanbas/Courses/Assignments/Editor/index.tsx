21f2f9b2b24bf7906fba02f5732155fbb6f5c319
linda <linnppa@gmail.com>
2024-02-24 20:06:22 -0500

    update edit assignment

--- src/Kanbas/Courses/Assignments/Editor/index.tsx
+++ src/Kanbas/Courses/Assignments/Editor/index.tsx
@@ -1,29 +1,149 @@
 import React from "react";
-import { useNavigate, useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { assignments } from "../../../Database";
+import { FaCheckCircle, FaEllipsisV } from "react-icons/fa";
+
 function AssignmentEditor() {
   const { assignmentId } = useParams();
-  const assignment = assignments.find(
-    (assignment) => assignment._id === assignmentId);
-  const { courseId } = useParams();
-  const navigate = useNavigate();
-  const handleSave = () => {
-    console.log("Actually saving assignment TBD in later assignments");
-    navigate(`/Kanbas/Courses/${courseId}/Assignments`);
+  const assignment = assignments.find((a) => a._id === assignmentId);
+
+  const smallContainerStyle = {
+    maxWidth: "70%",
+    margin: "0 auto",
   };
+
   return (
-    <div>
-      <h2>Assignment Name</h2>
-      <input value={assignment?.title}
-             className="form-control mb-2" />
-      <button onClick={handleSave} className="btn btn-success ms-2 float-end">
-        Save
-      </button>
-      <Link to={`/Kanbas/Courses/${courseId}/Assignments`}
-            className="btn btn-danger float-end">
-        Cancel
-      </Link>
+    <div className="container mt-3">
+      <div className="d-flex justify-content-between mb-3">
+        <h2>Edit Assignment</h2>
+
+        <span className="d-flex float-end">
+          <button
+            className="btn btn-outline-secondary btn-custom buttons"
+            style={{
+              border: "0px",
+              backgroundColor: "transparent",
+              color: "green",
+            }}
+          >
+            <FaCheckCircle className="me-2" style={{ color: "green" }} />
+            Publish
+          </button>
+          <button
+            style={{ width: "40px" }}
+            className="btn btn-outline-secondary btn-custom buttons"
+          >
+            <FaEllipsisV />
+          </button>
+        </span>
+      </div>
+      <hr className="horizontal-line"></hr>
+
+      <form>
+        {/* assignment Name */}
+        <div className="mb-3">
+          <label htmlFor="assignmentName" className="form-label">
+            Assignment Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="assignmentName"
+            defaultValue={assignment ? assignment.title : ""}
+            readOnly
+          />
+        </div>
+
+        {/* description */}
+        <div className="mb-3">
+          <label htmlFor="assignmentDescription" className="form-label">
+            Description
+          </label>
+          <textarea
+            className="form-control"
+            id="assignmentDescription"
+            rows={3}
+            placeholder="Description here..."
+            readOnly
+          ></textarea>
+        </div>
+
+        {/* smaller container */}
+        <div className="container" style={smallContainerStyle}>
+          {/* points */}
+          <div className="mb-3">
+            <label htmlFor="points" className="form-label">
+              Points
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="points"
+              defaultValue={assignment ? assignment.points : ""}
+              readOnly
+            />
+          </div>
+
+          {/* assignment group drop-down*/}
+          <div className="mb-3">
+            <label className="form-label">Assignment Group</label>
+            <select className="form-select" id="assignmentGroup">
+              <option> </option>
+              <option>Group 1</option>
+              <option>Group 2</option>
+              <option>Group 3</option>
+            </select>
+          </div>
+
+          {/* display grade as, drop down */}
+          <div className="mb-3">
+            <label className="form-label">Display Grade As</label>
+            <select className="form-select" id="displayGradeAs">
+              <option>Percentage</option>
+              <option>Points</option>
+              <option>Complete/Incomplete</option>
+            </select>
+          </div>
+
+          {/* due date */}
+          <div className="mb-3">
+            <label htmlFor="dueDate" className="form-label">
+              Due Date
+            </label>
+            <input
+              type="date"
+              className="form-control"
+              id="dueDate"
+              defaultValue={assignment ? assignment.dueDate : ""}
+              readOnly
+            />
+            </div>
+
+        </div>
+
+        {/* cancel & save */}
+        <div
+          className="d-flex justify-content-end"
+          style={{ marginLeft: "auto", padding: 5 }}
+        >
+          <button style={{ marginRight: 10 }}
+            type="button"
+            className="btn btn-outline-secondary btn-custom"
+          >
+            Cancel
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-secondary btn-custom-module"
+          >
+            Save
+          </button>
+          <br />
+          
+        </div>
+      </form>
     </div>
   );
 }
+
 export default AssignmentEditor;
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,35 +1,113 @@
 import React from "react";
-import { FaCheckCircle, FaEllipsisV, FaPlusCircle } from "react-icons/fa";
+import {
+  FaEllipsisV,
+  FaCheckCircle,
+  FaPlus,
+  FaHandPaper,
+  FaDotCircle,
+  FaGripVertical,
+} from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import { assignments } from "../../Database";
+import { FaPenToSquare } from "react-icons/fa6";
+import ".././index.css";
+
 function Assignments() {
   const { courseId } = useParams();
   const assignmentList = assignments.filter(
-    (assignment) => assignment.course === courseId);
+    (assignment) => assignment.course === courseId
+  );
+  const { dueDate } = useParams();
+
   return (
     <>
-      {/* Add buttons and other fields here */}
-      <ul className="list-group wd-modules">
+      {/* search bar & action buttons */}
+      <div className="d-flex justify-content-between mb-3" style={{padding: "10px"}}>
+        <input
+          type="search"
+          className="form-control btn-custom-searchbar"
+          placeholder="Search for Assignments"
+        />
+        <div>
+          <button className="btn btn-outline-secondary btn-custom">
+            <FaPlus /> Group
+          </button>
+          <button className="btn btn-outline-secondary btn-custom-module">
+            <FaPlus /> Assignment
+          </button>
+          <button className="btn btn-outline-secondary btn-custom-ellipses">
+            <FaEllipsisV />
+          </button>
+        </div>
+      </div>
+      <hr className="horizontal-line"></hr>
+
+      {/* assignments list */}
+      <ul className="list-group wd-modules" style={{ padding: "15px" }}>
         <li className="list-group-item">
           <div>
-            <FaEllipsisV className="me-2" /> ASSIGNMENTS
+            <FaEllipsisV style={{ color: "gray" }} className="me-2" />
+            <span style={{ fontWeight: "bold" }}>ASSIGNMENTS</span>
             <span className="float-end">
-              <FaCheckCircle className="text-success" />
-              <FaPlusCircle className="ms-2" /><FaEllipsisV className="ms-2" />
+              <span className="badge rounded-pill bg-light text-dark rounded-pill-big">
+                40% of total
+              </span>
+              <FaPlus style={{ color: "gray" }} className="ms-2" />
+              <FaEllipsisV style={{ color: "gray" }} className="ms-2" />
             </span>
           </div>
+
+          {/* assignments */}
           <ul className="list-group">
             {assignmentList.map((assignment) => (
-              <li className="list-group-item">
-                <FaEllipsisV className="me-2" />
-                <Link
-                   to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>{assignment.title}</Link>
-                <span className="float-end">
-                  <FaCheckCircle className="text-success" /><FaEllipsisV className="ms-2" /></span>
-              </li>))}
+              <li className="list-group-item d-flex align-items-center">
+                <div>
+                  <div className="d-flex align-items-center">
+                    <FaGripVertical
+                      style={{ color: "gray" }}
+                      className="me-2"
+                    />
+                    <FaPenToSquare
+                      style={{ color: "green" }}
+                      className="me-2"
+                    />
+                  </div>
+                </div>
+                <div className="flex-grow-1">
+                  <Link
+                    to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+                    className="link-custom"
+                    style={{
+                      marginLeft: "10px",
+                      color: "black",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    {assignment.title}
+                  </Link>
+                  <br />
+
+                  {/* subtext under assignment */}
+                  <span
+                    className="subtext"
+                    style={{
+                      marginLeft: "10px",
+                      marginRight: "5px",
+                    }}
+                  >
+                    <b>Due</b> {assignment.dueDate} | {assignment.points} points
+                  </span>
+                </div>
+                <div className="float-end">
+                  <FaCheckCircle className="text-success" />
+                  <FaEllipsisV style={{ color: "gray" }} className="ms-2" />
+                </div>
+              </li>
+            ))}
           </ul>
         </li>
       </ul>
     </>
-);}
+  );
+}
 export default Assignments;
