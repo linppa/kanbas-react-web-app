80d87024853ca314f6b1852118116e18b9be9ff9
linppa <67663326+linppa@users.noreply.github.com>
2024-04-20 12:32:57 -0400

    initial commit

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -40,7 +40,7 @@ function Dashboard(
         <br /><br />
       </div>
 
-      <h2>Published Courses (12)</h2> <hr />
+      <h2>Published Courses ({courses.length})</h2> <hr />
       <div className="row">
 
         <div className="row row-cols-1 row-cols-md-5 g-4">
